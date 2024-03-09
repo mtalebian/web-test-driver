124efa0f9a9e12fd11e496d5541286448c1f4056
ikco\81203770 <m.talebian@ikco.ir>
2024-03-09 19:33:03 +0330

    init

---
+++ WebTestDriver/ConsoleApp.Net46/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public decimal Price { get; set; }
+}
+
